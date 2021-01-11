c909d990d676854774070eab2ee5d2452aeeb15b
rckpwr <76976801+rckpwr@users.noreply.github.com>
2021-01-11 11:18:35 -0700

    Add files via upload

---
+++ hello_annotations.py
@@ -0,0 +1,7 @@
+# in folder of your hello open a new file and name it hello.py
+# this tells VS code that you are writing in Python
+# use msg to create a message and "" to create the string
+msg = "Hello World"
+# now that the string has been defined 
+# use print to call the string in the terminal
+print(msg)
