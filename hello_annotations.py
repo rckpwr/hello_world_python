f69f3028bb57f3206ee1d4a55a15677578ed2dc0
rckpwr <rck.pwr@gmail.com>
2021-01-11 12:31:50 -0700

    update annotations and explorations file

---
+++ hello_annotations.py
@@ -0,0 +1,15 @@
+# in folder of your hello open a new file and name it hello.py
+# this tells VS code that you are writing in Python
+# use msg to create a message and "" to create the string
+
+# this file has file type .py 
+# the file type .py tells the computer we are writing in python
+
+# to define a variable i can write any phrase that starts with a letter or _character and set it equal with an equal sign 
+# this variable "msg" has been set = to the data type string
+msg = "Hello World"
+# print is a native function to python
+# it prints the specified object to the screen
+# in this case the object is our msg
+print(msg)
+
---
+++ hello_explorations.py
@@ -0,0 +1,10 @@
+
+msg = "Hello World"
+msg2 = ["apples", "bananas", "pears", "kiwis"]
+msg3 = 20
+msg4 = ("apples", "bananas", "pears", "kiwis")
+msg5 = {"name" : "John", "age" : "42"}
+msg2.insert(0, "strawberries")
+msg2.append("watermelons")
+print(msg2)
+
